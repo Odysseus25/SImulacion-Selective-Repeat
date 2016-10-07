2b56325d1498c5654344b0a426b8153607c0e306
yuk27 <yuk2794@gmail.com>
2016-10-06 21:29:58 -0600

    Intermediario funcionando por hilos

--- Simulacion Selective Repeat/Python/Intermediariot.py
+++ Simulacion Selective Repeat/Python/Intermediariot.py
@@ -2,11 +2,13 @@
 import socket
 import sys
 import random
+import threading
+import thread
 
 port_cliente = 10000; 
 port_servidor = 10001; 
 
-proba_perdida = 50
+proba_perdida = 0
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,6 +22,38 @@ print >>sys.stderr, 'empezando a levantar %s puerto %s' % Cliente_address
 sock.bind(Cliente_address)
 sock.listen(1)
 
+def SendData():
+	try:
+		while True:
+		        data = connection.recv(13)
+		        if data: 
+		            print >>sys.stderr, 'recibido "%s"' % data
+		            sock_servidor.send(data)
+		            print >>sys.stderr, 'enviado'
+
+		        else:
+		            print >>sys.stderr, 'se acabo'
+		            break
+     	except Exception:
+        	import traceback
+        	print traceback.format_exc()
+	            
+
+def ReceiveData():
+   while True:
+        data = sock_servidor.recv(13)
+        if data:
+            ran = random.randint(1, 100) #se busca un numero random entre 1 y 100, de ser mayor al numero de proba de perdida se envia 
+            print >>sys.stderr, 'ran = "%s"' % ran
+            if ran > proba_perdida:  
+                print >>sys.stderr, 'enviando ack al cliente "%s"' % data
+                connection.send(data)
+            else:
+                print >>sys.stderr, 'se perdio el ack "%s"' % data
+        else:
+            print >>sys.stderr, 'no hay mas datos', client_address        
+           
+
 while True:
     # Esperando conexion
     print >>sys.stderr, 'Esperando para conectarse'
@@ -27,31 +61,13 @@ while True:
  
     try:
         print >>sys.stderr, 'concexion desde', client_address
- 
+        thread.start_new_thread(SendData, ())
+        thread.start_new_thread(ReceiveData, ())
         # Recibe los datos en trozos y reetransmite
-        while True:
-            data = connection.recv(13)
-            if data: 
-                print >>sys.stderr, 'recibido "%s"' % data
+        #while True:
 
-            else:
-                print >>sys.stderr, 'se acabo'
-                break
-
-            sock_servidor.send(data)
-           
-            data = sock_servidor.recv(13)
-            if data:
-                ran = random.randint(1, 100) #se busca un numero random entre 1 y 100, de ser mayor al numero de proba de perdida se envia 
-                print >>sys.stderr, 'ran = "%s"' % ran
-                if ran > proba_perdida:  
-                    print >>sys.stderr, 'enviando ack al cliente "%s"' % data
-                    connection.send(data)
-                else:
-                    print >>sys.stderr, 'se perdio el ack "%s"' % data
-            else:
-                print >>sys.stderr, 'no hay mas datos', client_address
                 #break
     finally:
         # Cerrando conexion
-        connection.close()
+        pass
+        #connection.close()
