7bf60ce26482af7268451d4f71dec23289a76037
Ulises Gonzalez <ulisesgz@outlook.com>
2016-10-08 13:22:46 -0600

    Agregado buffer de secuencia

--- Clientet.py
+++ Clientet.py
@@ -23,35 +23,65 @@ puerto = 10000
 modo = 0
 timeout = 3
 pos = 0
+posActualArchivo = 0	#donde estoy en el archivo original
+tamanoSec = ventana * 2
+posEnviar = 0	#posicion a enviar en la ventana
 array = [False for i in range(0,ventana)]
 
+#tamano de la secuencia a enviar, de ella se enviara uncamente la ventana.
+bufferSecuencia = [None]*(tamanoSec)
 
+#creacion del socket y conexion
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
 server_address = ('localhost', puerto)
 print >>sys.stderr, 'conectando a %s puerto %s' % server_address
 sock.connect(server_address)
-sock.setblocking(0)				
+sock.setblocking(0)		
 
-try:
+
+#llena el buffer con los caracteres que siguen del archivo
+def llenarBufferSecuencia(posActualArchivo):
+	for letra in range(0, tamanoSec):
+		bufferSecuencia[letra] = archivo[posActualArchivo]
+		posActualArchivo += 1 	
+		print (bufferSecuencia[letra])
 	
-	i = 0
+    	print (posActualArchivo)
 
-	while i < ventana:
-		dato = str((i,':',archivo[pos]))
-		print dato
-		sock.send(dato)
-		pos += 1
-		i += 1 
+#llenarBufferSecuencia(posActualArchivo)
 
-	espera =  time.time()
+#actualiza el buffer de secuencia con nuevos datos para ser enviados
+#lo que esta a la izquierda de la ventana se puede descartar y actualizar
+def actualizarBuffer(posActualizar, posActualArchivo):		
+	for letra in range(0, posActualizar):
+		bufferSecuencia[letra] = archivo[posActualArchivo]
+		posActualArchivo += 1 	
+		print (bufferSecuencia[letra])
+	
+    	print (posActualArchivo)
 
+#metodo enviar
+	try:
+	#envia la ventana sacada de bufferSecuencia
+		while i < ventana:
+			dato = str((i,':',bufferSecuencia[posEnviar]))
+			print dato
+			sock.send(dato)
+			posEnviar += 1
+			if posEnviar >= tamanoSec:	#para que circule sobre bufferSecuencia
+				posEnviar = 0
+			i += 1 
+
+		espera =  time.time()
+
+#Espera que le lleguen los acks, si se cumple el timeout, volver a mandarlo
+#actualizar bufferSecuencia
 	j = 0
 	while time.time() < espera + timeout and j < ventana:
 		ready = select.select([sock], [], [], timeout)
 		if ready[0]:
 			data = sock.recv(13)
 			if data: 
-				#
 				index = int(data[10])
 				if array[index] == False:	
 					print >>sys.stderr, 'llego "%s"'% data[10]	
