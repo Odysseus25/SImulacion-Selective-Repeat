de7a348c4d0550ad1eddc8d9a0dd933f0de8d042
Ulises Gonzalez <ulisesgz@outlook.com>
2016-10-09 22:54:58 -0600

    Cliente manda archivos de cualquier tamaño bien

--- Clientet.py
+++ Clientet.py
@@ -18,7 +18,7 @@ cantidad= len(sys.argv)
 	#				timeout = sys.argv[5]
 
 ventana = 5
-archivo = "Hello-World"
+archivo = "Hello-World, mucho te quiero"
 puerto = 10000
 modo = 0
 timeout = 3
@@ -26,10 +26,13 @@ pos = 0
 posActualArchivo = 0	#donde estoy en el archivo original
 tamanoSec = ventana * 2
 posEnviar = 0	#posicion a enviar en la ventana
-timeouts = [False for i in range(0,tamanoSec)]
-array = [False for i in range(0,tamanoSec)]
+timeouts = [False for i in range(0,tamanoSec)]	#maneja los timeouts de cada paquete
+array = [False for i in range(0,tamanoSec)]		#controla los acks recibidos
 caracteresEnviados = 0
-posInicialVentana = 0
+posInicialVentana = 0	#posicion inicial de la ventana
+cantidadMovida = 0		#cuanto se mueve la ventana
+posActualizar = 0
+controladorEnviados = [False for i in range(0,tamanoSec)]	#maneja cuales posiciones han sido enviadas
 
 #tamano de la secuencia a enviar, de ella se enviara uncamente la ventana.
 bufferSecuencia = [None]*(tamanoSec)
@@ -56,24 +59,37 @@ def llenarBufferSecuencia():
 #lo que esta a la izquierda de la ventana se puede descartar y actualizar
 #posInicial representa es la posicion donde inicia la ventana, o sea, posEnviar
 def actualizarBuffer(posInicial):
+	print('pos incial')
+	print(posInicial)
+	global controladorEnviados
 	global posActualArchivo
-	inicio = (posInicial + ventana) % tamanoSec		#define de donde a donde actualizar el buffer
+	global posActualizar
+
 	i = 0
-	while i < ventana:
-		bufferSecuencia[inicio] = archivo[posActualArchivo]
-		posActualArchivo += 1 	
-		inicio += 1
+	while i < cantidadMovida:		#actualiza las posiciones que ya tienen ack
+		if posActualArchivo < len(archivo):
+			bufferSecuencia[posActualizar] = archivo[posActualArchivo]
+			controladorEnviados[posActualizar] = False		#resetea la posicion a no enviada
+			posActualArchivo += 1 	
+			posActualizar += 1
+			if posActualizar >= tamanoSec:
+				posActualizar = 0
 		i += 1
-		print (bufferSecuencia[inicio])
-	
-    	print (posActualArchivo)
+
+    	#print (posActualArchivo)
+		print(bufferSecuencia)
 
 #determina el numero de posiciones que la ventana se debe mover a la derecha
 #pos es la posicion donde inicia la ventana, o sea, posInicialVentana
 def moverVentana(pos):
+	#print('pos ventana')
+	#print(pos)
 	global posInicialVentana
+	global cantidadMovida
 	global caracteresEnviados
 	global posEnviar
+
+	cantidadMovida = 0
 	acks = 0
 	i = 0
 	while i < ventana and array[pos] == True:	#cuenta cuantos acks se han recibido seguidos
@@ -82,8 +98,10 @@ def moverVentana(pos):
 		if pos >= tamanoSec:
 			pos = 0
 		i += 1
+
 	posInicialVentana += acks	#corre la ventana el numero de acks recibidos
 	caracteresEnviados += acks 	#aumenta el numero de caracteres enviados
+	cantidadMovida += acks
 	if posInicialVentana >= tamanoSec:	#que sea ciclico
 		posInicialVentana = posInicialVentana % tamanoSec
 	posEnviar = posInicialVentana 	#actualiza la nueva posicion a mandar
@@ -112,11 +130,17 @@ try:
 	while caracteresEnviados < len(archivo):
 		k = 0
 
+		if primeraCorrida == False:
+			actualizarBuffer(posInicialVentana)
+
 		while k < ventana:		#mientras hayan datos por enviar en la ventana
-			dato = str(str(posEnviar)+ ':' + str(bufferSecuencia[posEnviar])) + '\n' #envia el numero de paquete y el caracter difinido en esa posicion
-			print dato
-			sock.send(dato)
-			timeouts[posEnviar] = time.time()	#se pone el timeout del dato recien enviados
+			if controladorEnviados[posEnviar] == False:
+				dato = str(str(posEnviar)+ ':' + str(bufferSecuencia[posEnviar])) + '\n' #envia el numero de paquete y el caracter difinido en esa posicion
+				print dato
+				sock.send(dato)
+				controladorEnviados[posEnviar] = True
+				timeouts[posEnviar] = time.time()	#se pone el timeout del dato recien enviados
+			
 			posEnviar += 1
 			if posEnviar >= tamanoSec:	#para que circule
 				posEnviar = 0
@@ -124,20 +148,21 @@ try:
 
 		#ready = select.select([sock], [], [], timeout)	#si se puede recibir
 		#if ready[0]:
-			servidor_response = sock.recv(1) #recibe el ack del intermediario 
-			if servidor_response: #si hay respuesta 
-				rec =''
-				while servidor_response and servidor_response !='\n':
-					rec += 	servidor_response
-					servidor_response = sock.recv(1)
+		servidor_response = sock.recv(1) #recibe el ack del intermediario 
+		if servidor_response: #si hay respuesta 
+			rec =''
+			while servidor_response and servidor_response !='\n':
+				rec += 	servidor_response
+				servidor_response = sock.recv(1)
 
-				servidor_response = rec
-				ack = int(servidor_response)	#guarda el ack
-				if array[ack] == False:		#si todavia no ha llegado ese ack
-					print >> sys.stderr, 'llego "%s"' %servidor_response
-					array[ack] = True
-					moverVentana(posInicialVentana)		#mueve el valor de la posicion incial de la ventana al primero qyue esta en False
+			servidor_response = rec
+			ack = int(servidor_response)	#guarda el ack
+			if array[ack] == False:		#si todavia no ha llegado ese ack
+				print >> sys.stderr, 'llego "%s"' %servidor_response
+				array[ack] = True
+				moverVentana(posInicialVentana)		#mueve el valor de la posicion incial de la ventana al primero qyue esta en False
 
+		#print ('cuantas veces hago esto')
 		CheckTimeout(posInicialVentana, ventana)
 		primeraCorrida = False
 
